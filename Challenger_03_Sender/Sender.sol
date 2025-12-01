f216e684f2c8d09962355c205436034863da6054
Jorge Paez D. <123193010+Jpaezd@users.noreply.github.com>
2025-11-30 21:29:38 -0500

    Create Sender.sol

---
+++ Challenger_03_Sender/Sender.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+pragma solidity 0.8.24;
+
+/**
+ * @title Sender
+ * @notice Basic contract used to store the address of the deployer.
+ * @dev Demonstrates constructor execution and msg.sender usage.
+ */
+contract Sender {
