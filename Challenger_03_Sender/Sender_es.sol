7ebb4ea613deb65904087a438eaf4d78a33ec0f1
Jorge Paez D. <123193010+Jpaezd@users.noreply.github.com>
2025-11-30 21:28:00 -0500

    Create Sender_es.sol

---
+++ Challenger_03_Sender/Sender_es.sol
@@ -0,0 +1,7 @@
+  // SPDX-License-Identifier: LGPL-3.0-only
+// Indica la licencia del contrato. LGPL-3.0 permite usar el código incluso
+// en proyectos cerrados mientras mantengas los cambios abiertos.
+
+// Versión del compilador Solidity requerida.
+// Se usa 0.8.24 por sus mejoras de seguridad y overflow checks integrados.
+pragma solidity 0.8.24;
